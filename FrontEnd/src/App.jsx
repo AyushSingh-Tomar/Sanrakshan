efa1572fae25371e4b00d17e8c1edb8b065bc5da
vedantsrivastava42 <vedantsrivastava42@gmail.com>
2024-09-06 02:07:54 +0530

    Signup login popup

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar} from "./Components/NavBar/NavBar"
 import {Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home'
@@ -10,17 +10,21 @@ import Footer from './Components/Footer/Footer'
 import { useState } from "react"
 
 const App = () => {
-  const [showLogin,setShowLogin]=useState(false)
+  const [showLogin,setShowLogin]=useState(false);
+  const toggleLoginPopup = () => {
+    setShowLogin(!showLogin);
+  };
   return (
     <>
-    {showLogin?<LoginPopup/>:<></>}
+    {/* {showLogin?<LoginPopup/>:<></>} */}
+    {showLogin && <LoginPopup toggleDialog={toggleLoginPopup}/>} 
     <div className='app'>
-    <Navbar/>
+    <Navbar toggleLoginPopup={toggleLoginPopup}/>
     <Routes>
-<Route path="/" element={<Home/>}/>
-<Route path="/dashboard" element={<DashBoard/>}/>
-<Route path="/trackProgress" element={<TrackProgress/>}/>
-<Route path="/applynoc" element={<ApplyNOC/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/dashboard" element={<DashBoard/>}/>
+      <Route path="/trackProgress" element={<TrackProgress/>}/>
+      <Route path="/applynoc" element={<ApplyNOC/>}/>
     </Routes>
     </div>
     <Footer/>
